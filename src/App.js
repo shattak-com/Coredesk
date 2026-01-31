14af3ce2358106df6a7bef3759a6d487192507f5
Prem_Mule <muleprem777@gmail.com>
2026-01-31 12:25:49 +0530

    second test commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Courses from "./Pages/Courses";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        
+        <Route path="/" element={<Home />} />
         <Route path="/courses" element={<Courses />} />
       </Routes>
     </BrowserRouter>
