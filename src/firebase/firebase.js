1bf2f57ae3e833c36d659bce39416dbb551afa76
Prem_Mule <muleprem777@gmail.com>
2026-01-31 12:22:11 +0530

    second test commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,7 +12,7 @@ const firebaseConfig = {
   storageBucket: "shattak-prod.firebasestorage.app",
   messagingSenderId: "130414227088",
   appId: "1:130414227088:web:fab8f1ed8b461d8fb13215",
-  measurementId: "G-H4N0ML8Q4K"
+  measurementId: "G-H4N0ML8Q4K",
 };
 
 // Initialize Firebase
