9f5fa5a4c7729a5f7a8e47d9aa7dc981d8e346a0
Prem_Mule <muleprem777@gmail.com>
2026-01-31 12:30:18 +0530

    third test commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Courses from "./Pages/Courses";
 import Home from "./Pages/Home";
 
