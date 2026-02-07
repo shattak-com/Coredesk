17a8ea5f1394ee7432097e246dac26361d2e17ea
Prem_Mule <muleprem777@gmail.com>
2026-02-07 18:06:05 +0530

    env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,13 +9,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-  // apiKey: "AIzaSyDM8ZK5YJHe4vtj7kkSRhu-pGeMx9IjdaU",
-  // authDomain: "shattak-prod.firebaseapp.com",
-  // projectId: "shattak-prod",
-  // storageBucket: "shattak-prod.firebasestorage.app",
-  // messagingSenderId: "130414227088",
-  // appId: "1:130414227088:web:fab8f1ed8b461d8fb13215",
-  // measurementId: " G-H4N0ML8Q4K",
 };
 
 console.log(firebaseConfig);
